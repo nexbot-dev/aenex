b5403096d559b1948158c21fb958a90c4bdd0484
aenjojo <josuafernando999@gmail.com>
2023-07-10 23:42:44 +0700

    fix: update app to use nex-framework

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
 import dotenv from 'dotenv';
 import { GatewayIntentBits } from 'discord.js';
-import { NexClient } from '#core/NexClient';
+import { AenexClient } from '#core/AenexClient';
+import { SetDB } from '#databases/SetDB';
 
 dotenv.config();
 
-(function main() {
+(async function main() {
 	const botToken = process.env.DISCORD_BOT_TOKEN;
 
 	if (!botToken) return;
 
-	const client = new NexClient({
+	const client = new AenexClient({
 		intents: [GatewayIntentBits.Guilds],
 	});
 
 	const registry = client.registry;
 
-	registry.registerEvents();
-	registry.registerCommands();
+	registry.registerEvents('./events');
+	registry.registerCommands('./commands');
+
+	client.db = await SetDB.connect();
 
 	client.login(botToken);
 })();

a4f3870d2838448cb42d0914c5aafb7f1ebb496e
aenjojo <josuafernando999@gmail.com>
2023-07-10 23:36:30 +0700

    refactor: change to Deployer

--- scripts/deploy-commands.dev.js
+++ scripts/deploy-commands.dev.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
-import { NexDeploy } from '../dist/core/NexDeploy.js';
+import { Deployer } from '../dist/core/Deployer.js';
 
 dotenv.config();
 
 (async function deployCommands() {
-	const deployment = new NexDeploy(process.env.DISCORD_BOT_TOKEN);
+	const deployment = new Deployer(process.env.DISCORD_BOT_TOKEN);
 
 	(await deployment.loadCommands()).deployToServer();
 })();
